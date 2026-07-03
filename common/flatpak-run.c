IGS-GIT/flatpak

commit c4fce9e42abe7b29d133937381fa6582b66433bc

run: Error out if file forwarding of empty paths is attempted
The file forwarding feature errors out when the path or URI does not
lead to an actual file that can be forwarded. The empty path never
describes an actual file, so we always have to error out.

Without the check, we would get a NULL path from
`flatpak_file_get_path_cached` and crash later.

Closes: #6689

common/flatpak-run.c
@@ -2724,6 +2724,9 @@ add_rest_args (FlatpakBwrap   *bwrap,
             }
           else
             file = g_file_new_for_path (args[i]);
+
+          if (flatpak_file_get_path_cached (file) == NULL)
+            return flatpak_fail (error, _("Invalid path ‘%s’"), args[i]);
         }
 
       if (file && !flatpak_exports_path_is_visible (exports,

commit b8c787924a6e595448178b2e8adc3fb3438281db

run: Validate file object before checking cached path
Verify that the file object is valid before checking its cached path
to avoid a potential NULL pointer dereference.

Fixes: c4fce9e4 ("run: Error out if file forwarding of empty paths is attempted")

common/flatpak-run.c
@@ -2725,7 +2725,7 @@ add_rest_args (FlatpakBwrap   *bwrap,
           else
             file = g_file_new_for_path (args[i]);
 
-          if (flatpak_file_get_path_cached (file) == NULL)
+          if (file && flatpak_file_get_path_cached (file) == NULL)
             return flatpak_fail (error, _("Invalid path ‘%s’"), args[i]);
         }
 